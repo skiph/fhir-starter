91b8a82345f876bf1916866da405fc80fa1e81fc
skiph <skiph@maplerise.com>
2021-05-23 18:02:01 -0700

    Add patient json display

--- src/components/Patient.js
+++ src/components/Patient.js
@@ -1,58 +1,66 @@
-import React from "react";
-import { FhirClientContext } from "../FhirClientContext";
+import React from 'react';
+import { FhirClientContext } from '../FhirClientContext';
+import ReactJson from 'react-json-view';
 
 function PatientName({ name = [] }) {
-    let entry =
-        name.find(nameRecord => nameRecord.use === "official") || name[0];
-    if (!entry) {
-        return <h1>No Name</h1>;
-    }
-    return <h1>{entry.given.join(" ") + " " + entry.family}</h1>;
+  let entry = name.find(nameRecord => nameRecord.use === 'official') || name[0];
+  if (!entry) {
+    return <h1>No Name</h1>;
+  }
+  return <h1>{entry.given.join(' ') + ' ' + entry.family}</h1>;
 }
 
 function PatientBanner(patient) {
-    return (
-        <div>
-            <PatientName name={patient.name} />
-            <span>
-                Gender: <b>{patient.gender}</b>,{" "}
-            </span>
-            <span>
-                DOB: <b>{patient.birthDate}</b>
-            </span>
-        </div>
-    );
+  return (
+    <div>
+      <PatientName name={patient.name} />
+      <span>
+        Gender: <b>{patient.gender}</b>,{' '}
+      </span>
+      <span>
+        DOB: <b>{patient.birthDate}</b>
+      </span>
+    </div>
+  );
 }
 
 export default class Patient extends React.Component {
-    static contextType = FhirClientContext;
-    constructor(props) {
-        super(props);
-        this.state = {
-            loading: true,
-            patient: null,
-            error: null
-        };
+  static contextType = FhirClientContext;
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: true,
+      patient: null,
+      error: null,
+    };
+  }
+  componentDidMount() {
+    const client = this.context.client;
+    this._loader = client.patient
+      .read()
+      .then(patient => {
+        this.setState({ patient, loading: false, error: null });
+      })
+      .catch(error => {
+        this.setState({ error, loading: false });
+      });
+  }
+  render() {
+    const { error, loading, patient } = this.state;
+    if (loading) {
+      return null;
     }
-    componentDidMount() {
-        const client = this.context.client;
-        this._loader = client.patient
-            .read()
-            .then(patient => {
-                this.setState({ patient, loading: false, error: null });
-            })
-            .catch(error => {
-                this.setState({ error, loading: false });
-            });
+    if (error) {
+      return error.message;
     }
-    render() {
-        const { error, loading, patient } = this.state;
-        if (loading) {
-            return null;
-        }
-        if (error) {
-            return error.message;
-        }
-        return <PatientBanner {...patient} />;
-    }
+    return (
+      <div>
+        <PatientBanner {...patient} />
+        <div style={{ padding: '25px 25px' }}>
+          <ReactJson src={patient} name="patient" collapsed={true} />
+          {/* <pre>{JSON.stringify(patient, null, 2)}</pre> */}
+        </div>
+      </div>
+    );
+  }
 }
